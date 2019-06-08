28898ba420e575782825e7f64d11076ee602c2c0
Vitória Butzke <vivipiccobr@gmail.com>
2019-06-08 17:14:00 -0300

    Fix environment dependencies

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -6,11 +6,14 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include "Shader.h"
+#include <string>
+#include <assert.h>
 
 using namespace std;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 GLFWwindow* init();
+void draw();
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
@@ -48,7 +51,9 @@ int main(int argc, char **argv)
 			result += sendBuffer[i];
 		}
 		printf("Result is: %d", result);
+		draw();
 		//loop here
+		glfwTerminate();
 	}	
 
 	
@@ -116,4 +121,48 @@ GLFWwindow* init() {
 	glfwGetFramebufferSize(window, &width, &height);
 	glViewport(0, 0, width, height);
 	return window;
+}
+
+void draw() {
+	GLFWwindow* window = init();
+	Shader shader("../shaders/shader.vs", "../shaders/shader.frag");
+	
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f, // Left  
+		0.5f, -0.5f, 0.0f, // Right 
+		0.0f,  0.5f, 0.0f  // Top   
+	};
+	
+	GLuint VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	
+	GLint colorLoc = glGetUniformLocation(shader.Program, "inputColor");
+	assert(colorLoc > -1);
+	glUseProgram(shader.Program);
+	glUniform4f(colorLoc, 1.0f, 0.0f, 1.0f, 1.0f);
+	
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		shader.Use();
+	
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+		glfwSwapBuffers(window);
+	}
+	
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glfwTerminate();
+	//return 0;
 }
